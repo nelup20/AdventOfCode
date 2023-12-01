dca9f4491b535633bddf0d0acb6e70f0a41715b3
Nelu Platonov <nelu.platonov@gmail.com>
2023-12-01 20:19:07 +0100

    2023: Day 1 - part 1 done

---
+++ 2023 (Java)/Day 1/part1.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class part1 {
+    public static Pattern numberRegex = Pattern.compile("\\d");
+
+    public static void main(String[] args) throws FileNotFoundException {
+        List<Integer> numbers = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(new File("input.txt"))) {
+            while (scanner.hasNext()) {
+                ArrayList<String> lineChars = new ArrayList<>(List.of(scanner.nextLine().split("")));
+
+                int firstNumber = findFirstInteger(lineChars);
+
+                Collections.reverse(lineChars);
+                int lastNumber = findFirstInteger(lineChars);
+
+                numbers.add(Integer.parseInt(firstNumber + "" + lastNumber));
+            }
+        }
+
+        System.out.println("Result: " + numbers.stream().reduce(Integer::sum).get());
+    }
+
+    private static int findFirstInteger(List<String> list) {
+        return list.stream()
+                   .filter(character -> numberRegex.matcher(character).matches())
+                   .mapToInt(Integer::parseInt)
+                   .findFirst()
+                   .orElseThrow();
+    }
+}
